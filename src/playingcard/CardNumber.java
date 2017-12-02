d613db5f8885c73681dc504b7eca39a9baab747b
kazusa4418 <astarte.rododaktylos@gmail.com>
2017-12-02 21:37:31 +0900

    playingcard.CardNumberクラスにjavadocを追加しました。

--- src/playingcard/CardNumber.java
+++ src/playingcard/CardNumber.java
@@ -1,5 +1,13 @@
 package playingcard;
 
+/**
+ * この列挙型クラスはトランプにおける数字を実現するクラスです。
+ * @author kazusa4418
+ * @see Card
+ * @see CardMark
+ * @since 1.0
+ * @deprecated Cardクラスを利用してください
+ */
 public enum CardNumber {
     num1("A"),           //ordinal is 0
     num2("2"),           //ordinal is 1
@@ -16,12 +24,28 @@ public enum CardNumber {
     num13("K"),          //ordinal is 12
     JOKER("JOKER");      //ordinal is 13
 
+    /**
+     * このEnumクラスのインスタンスのトランプの数字を持つString型変数です。
+     * 各数字に対応する文字列または数字をStringとして格納しています。
+     */
     private String number;
 
+    /**
+     * このコンストラクタはこのEnumのインスタンスが作られた際にString型の変数を受け取り
+     * フィールドであるnumberへ格納します。
+     *
+     * @param number - 受け取った文字列はこのインスタンスの数字の見た目になります。
+     */
     CardNumber(String number) {
         this.number = number;
     }
 
+    /**
+     * このインスタンスの持つ数字の見た目をStringで返却するメソッドです。
+     * num1のインスタンスであればAを返し、num2であれば2を返します。
+     *
+     * @return このインスタンスの数字の見た目を返します。
+     */
     public String toString() {
         return this.number;
     }
